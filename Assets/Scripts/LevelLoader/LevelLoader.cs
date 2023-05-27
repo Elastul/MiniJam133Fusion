1fc598a58a3f83e44c024426b56a6a82d0c9bf8e
JaxterMG <46417741+JaxterMG@users.noreply.github.com>
2023-05-27 21:00:06 +0300

    TestLevel + levelloader

---
+++ Assets/Scripts/LevelLoader/LevelLoader.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private int _nextLevel;
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer.Equals(LayerMask.NameToLayer("Player")))
+        {
+            LoadLevel();
+        }
+    }
+    private void LoadLevel()
+    {
+        SceneManager.LoadScene(_nextLevel);
+    }
+}
