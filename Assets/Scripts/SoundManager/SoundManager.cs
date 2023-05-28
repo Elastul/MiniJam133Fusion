233a9db32a67709246c03dee2a4d60147b6953e5
JaxterMG <46417741+JaxterMG@users.noreply.github.com>
2023-05-28 13:43:42 +0300

    Sound manager and sound library

--- Assets/Scripts/SoundManager/SoundManager.cs
+++ Assets/Scripts/SoundManager/SoundManager.cs
@@ -1,20 +1,48 @@
-using System.Collections.Generic;
 using UnityEngine;
-using Sirenix.OdinInspector;
+
 
 public class SoundManager : MonoBehaviour
 {
     private AudioSource[] _audioSources;
+    public SoundLibrary _soundLibrary;
+    public static SoundManager Instance;
 
-    public static SoundManager Instance; 
-
-    [ShowInInspector]
-    public Dictionary<string, AudioClip> _soundLibraty = new Dictionary<string, AudioClip>();
-
-    private void Start()
+    private void Awake()
     {
-
         Instance = this;
         _audioSources = GetComponents<AudioSource>();
+    }
+
+    public void RequestSound(string soundName)
+    {
+        var source = ChooseUnoccupiedSource();
+        var clip = FindClip(soundName);
+        PlaySound(source, clip);
+    }
+
+    private AudioSource ChooseUnoccupiedSource()
+    {
+        foreach (var source in _audioSources)
+        {
+            if (!source.isPlaying) return source;
+        }
+
+        var newSource = gameObject.AddComponent<AudioSource>();
+        AudioSource[] temp = new AudioSource[_audioSources.Length + 1];
+        temp[temp.Length - 1] = newSource;
+
+        _audioSources = temp;
+        return newSource;
+    }
+
+    private AudioClip FindClip(string soundName)
+    {
+        _soundLibrary._soundLibraty.TryGetValue(soundName, out var clip);
+        return clip;
+    }
+
+    private void PlaySound(AudioSource source, AudioClip clip)
+    {
+        source.PlayOneShot(clip);
     }
 }
