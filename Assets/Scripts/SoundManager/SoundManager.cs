7fc16dfb1dd04c817241549e205b2651e4136aa8
JaxterMG <46417741+JaxterMG@users.noreply.github.com>
2023-05-28 12:55:33 +0300

    Integrated odin inspector and sounds

--- Assets/Scripts/SoundManager/SoundManager.cs
+++ Assets/Scripts/SoundManager/SoundManager.cs
@@ -1,15 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 public class SoundManager : MonoBehaviour
 {
-    public AudioClip[] StepSounds;
-    public float StepInterval = 0.5f;
     private AudioSource[] _audioSources;
 
     public static SoundManager Instance; 
 
+    [ShowInInspector]
+    public Dictionary<string, AudioClip> _soundLibraty = new Dictionary<string, AudioClip>();
+
     private void Start()
     {
+
         Instance = this;
         _audioSources = GetComponents<AudioSource>();
     }
